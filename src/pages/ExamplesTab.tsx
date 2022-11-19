6e316b977af91e90b956b8b37166d6e791c21eff
CarmineBlefari00 <91595073+CarmineBlefari00@users.noreply.github.com>
2022-11-19 11:01:27 +0100

    Create ExamplesTab
    tab create to introduce the created Examples page

---
+++ src/pages/ExamplesTab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Examples from "../components/Examples";
+
+const ExamplesTab: React.FC = () => {
+  return (
+  
+	<Examples />
+  );
+};
+
+export default ExamplesTab;
