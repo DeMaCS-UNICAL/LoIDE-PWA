16507ea3bc88bf59e95d9945a536a471625ac9ca
CarmineBlefari00 <91595073+CarmineBlefari00@users.noreply.github.com>
2022-11-29 11:58:41 +0100

    Create introSteps.js
    File to avoide use of steps in the main page

---
+++ src/lib/introSteps.js
@@ -0,0 +1,40 @@
+const introSteps = [
+	{
+      title: 'Welcome',
+      intro: 'Lets see together LoIDE application',
+      tooltipClass: 'myTooltipClass',
+      highlightClass: 'myHighlightClass',
+    },
+    {
+      element: '#runButton',
+      title: 'Run Button',
+      intro: 'Allows you to run your project, just click on it and you will see the output in the corresponding tab',
+    },
+    {
+      element: '#openButtonId',
+      title: 'Open Button',
+      intro: 'Clicking on this button you can choose files and drop in the corresponding area',
+    },
+    {
+      element: '#saveButton',
+      title: 'Save Button',
+      intro: 'Here you can save your project and download it',
+    },
+    {
+      element: '#shareButton',
+      title: 'Share Button',
+      intro: 'Gives you the possibility of sharing your project through the link',
+    },
+    {
+      element: '#resetButton',
+      title: 'Reset Button',
+      intro: 'Very useful button with different tools: Reset actions, input and project',
+    },
+    {
+      element: '#operationsTab',
+      title: 'Operations Tab',
+      intro: 'Here you can interact with your project editor',
+    },
+];
+
+export default introSteps;
