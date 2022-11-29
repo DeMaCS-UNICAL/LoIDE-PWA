29a9505cd293127c3f8b1266aad2991989e067b1
CarmineBlefari00 <91595073+CarmineBlefari00@users.noreply.github.com>
2022-11-29 12:05:38 +0100

    Update MainTab.tsx
    Importing intro js steps

--- src/pages/MainTab.tsx
+++ src/pages/MainTab.tsx
@@ -40,6 +40,7 @@ import Mousetrap from "mousetrap";
 
 import { Steps } from "intro.js-react";
 import "intro.js/introjs.css";
+import introSteps from '../lib/introSteps.js';
 
 type MainTabPageProps = RouteComponentProps<{
   data: string;
@@ -144,45 +145,6 @@ const MainTab: React.FC<MainTabPageProps> = ({ match }) => {
       })
       .then((alert) => alert.present());
   };
-
-   const steps = [
-    {
-      title: 'Welcome',
-      intro: 'Lets see together LoIDE application',
-      tooltipClass: 'myTooltipClass',
-      highlightClass: 'myHighlightClass',
-    },
-    {
-      element: '#runButton',
-      title: 'Run Button',
-      intro: 'Allows you to run your project, just click on it and you will see the output in the corresponding tab',
-    },
-    {
-      element: '#openButtonId',
-      title: 'Open Button',
-      intro: 'Clicking on this button you can choose files and drop in the corresponding area',
-    },
-    {
-      element: '#saveButton',
-      title: 'Save Button',
-      intro: 'Here you can save your project and download it',
-    },
-    {
-      element: '#shareButton',
-      title: 'Share Button',
-      intro: 'Gives you the possibility of sharing your project through the link',
-    },
-    {
-      element: '#resetButton',
-      title: 'Reset Button',
-      intro: 'Very useful button with different tools: Reset actions, input and project',
-    },
-    {
-      element: '#operationsTab',
-      title: 'Operations Tab',
-      intro: 'Here you can interact with your project editor',
-    },
-    ];
   
   const onExit = () => {}
 
@@ -213,7 +175,7 @@ const MainTab: React.FC<MainTabPageProps> = ({ match }) => {
 
 	   <IonButtons slot="end">
 
-         <Steps enabled={true} steps={steps} initialStep={0} onExit={onExit} options={options} />
+         <Steps enabled={true} steps={introSteps} initialStep={0} onExit={onExit} options={options} />
 
             <IonButton
 	      id = "openButtonId"
