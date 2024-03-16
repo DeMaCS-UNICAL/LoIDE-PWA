a5f381e6b2e878a5661c85a9f3cb2c0cce4265c3
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-03-17 00:48:25 +0100

    Fixed inefficient regular expression
    See https://github.com/DeMaCS-UNICAL/LoIDE-PWA/security/code-scanning/1

--- src/components/LoideAceEditor.tsx
+++ src/components/LoideAceEditor.tsx
@@ -481,12 +481,12 @@ const LoideAceEditor = React.forwardRef<AceEditor, LoideAceEditorProps>((props, 
     switch (languageChosen) {
       case LoideLanguages.ASP.name:
       case LoideLanguages.DATALOG.name: {
-        const splitRegex = /(([a-zA-Z_]+[0-9]*)*)(\(.+?\))/gi;
+        const splitRegex = /([a-zA-Z_]\w*)(\(.+?\))/gi;
         const words = editorText.match(splitRegex);
         if (words != null) {
           const map = new Map();
           words.forEach(function (word: string) {
-            const nameMatches = word.match(/[^_](([a-zA-Z_]+[0-9]*)*)/);
+            const nameMatches = word.match(/[^_]([a-zA-Z_]\w*)/);
             const aritiesMathces = word.match(/\(.+?\)/);
             if (nameMatches && aritiesMathces) {
               const name = nameMatches[0];
