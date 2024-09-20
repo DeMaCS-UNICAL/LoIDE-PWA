1721eda8225a018b8c4a59c1aee98f8b106f697c
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-09-21 00:40:14 +0200

    Update Option.test.tsx

--- src/__tests__/components/Option.test.tsx
+++ src/__tests__/components/Option.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { fireEvent } from '@testing-library/react';
+import { fireEvent } from "@testing-library/react";
 import Option from "../../components/Option";
 import { IOptionsData } from "../../lib/LoideAPIInterfaces";
 import { ISolverOption } from "../../lib/LoideInterfaces";
