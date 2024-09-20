a7e2c63046d269b53cb8660e5b3a3f87e18edc54
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-09-21 00:16:21 +0200

    Removing ionic-react-test-utils

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,8 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import "@testing-library/jest-dom/extend-expect";
-import { mockIonicReact } from "@ionic/react-test-utils";
-mockIonicReact();
+
++ import { setupIonicReact } from "@ionic/react";
+
++ setupIonicReact();
+
+// Mock matchmedia
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    addListener: function () { },
+    removeListener: function () { }
+  };
+};
+
+
 // @ts-ignore
+
 global.setImmediate = jest.useRealTimers;
