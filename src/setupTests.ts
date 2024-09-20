daf4e5931802edf27e835057c61f6991f741dbae
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-09-21 00:29:47 +0200

    Minor

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,12 +6,14 @@ import "@testing-library/jest-dom/extend-expect";
 import { setupIonicReact } from "@ionic/react";
 setupIonicReact();
 // Mock matchmedia
-window.matchMedia = window.matchMedia || function () {
-  return {
-    matches: false,
-    addListener: function () {},
-    removeListener: function () {}
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
   };
-};
 // @ts-ignore
 global.setImmediate = jest.useRealTimers;
