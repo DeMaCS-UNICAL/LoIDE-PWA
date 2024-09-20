1a18282c55441c47174bc7a815f2873cf1f1e708
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2024-09-21 00:38:45 +0200

    Update Appearance.test.tsx

--- src/__tests__/components/Appearance.test.tsx
+++ src/__tests__/components/Appearance.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Appearance from "../../components/Appearance";
-import { fireEvent } from '@testing-library/react';
+import { fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../redux";
 
