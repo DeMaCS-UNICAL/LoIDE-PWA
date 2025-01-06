785119ecd29f3f050cd98270d90f57ad35b83a9c
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2025-01-06 23:09:20 +0100

    Update Appearance.test.tsx

--- src/__tests__/components/Appearance.test.tsx
+++ src/__tests__/components/Appearance.test.tsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import Appearance from "../../components/Appearance";
-import { ionFireEvent } from "@ionic/react-test-utils";
 import { Provider } from "react-redux";
 import { store } from "../../redux";
 
@@ -36,7 +36,7 @@ describe("<Appearance />", () => {
     );
     await screen.findByText("Dark mode");
     const toogle = await screen.findByTitle("Toogle dark mode");
-    ionFireEvent.ionChange(toogle, "1");
+    fireEvent.change(toogle, "1");
   });
 
   it("test font size editor item", async () => {
@@ -48,7 +48,7 @@ describe("<Appearance />", () => {
 
     const range = await screen.findByTitle("Font size editor range");
     const labelTexts = screen.queryAllByText("Font size");
-    ionFireEvent.ionChange(range, "1");
+    fireEvent.change(range, "1");
 
     expect(labelTexts.length).toBeGreaterThanOrEqual(1);
   });
@@ -63,12 +63,13 @@ describe("<Appearance />", () => {
 
     const range = await screen.findByTitle("Font size output range");
     const labelTexts = screen.queryAllByText("Font size");
-    ionFireEvent.ionChange(range, "1");
+    fireEvent.change(range, "1");
 
     expect(labelTexts.length).toBeGreaterThanOrEqual(1);
   });
 
   it("test reset button", async () => {
+    const user = userEvent.setup()
     Object.defineProperty(window, "matchMedia", {
       writable: true,
       value: jest.fn().mockImplementation((query) => ({
@@ -90,6 +91,6 @@ describe("<Appearance />", () => {
     );
 
     const button = await screen.findByTitle("Reset appearance options");
-    ionFireEvent.click(button);
+    await user.click(button);
   });
 });
