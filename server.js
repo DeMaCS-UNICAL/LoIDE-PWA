946ad90d2107720b4b3f7839cc0119633a5a8847
Stefano Germano <2128484+stefanogermano@users.noreply.github.com>
2025-10-05 23:19:10 +0200

    Hotfix Express 5
    Solved `PathError` problem as described in: https://expressjs.com/en/guide/migrating-5.html#path-syntax

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
