fa89b72474f97b7007864099541fbf14dc8c5ae3
Rocco Palermiti <roccopalermiti.rp@gmail.com>
2025-11-09 19:43:50 -0500

    fix: add server configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true, // automatically open browser
+  },
   build: {
     outDir: "build", // CRA's default build output
   },
