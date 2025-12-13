29a5e055bbd02d6fa302c516afbbf3ca89bad427
Rocco Palermiti <roccopalermiti.rp@gmail.com>
2025-12-12 23:02:09 -0500

    Refactor useOutput hook to utilize useCallback for performance optimization and enhance output state management

--- src/hooks/useOutput.ts
+++ src/hooks/useOutput.ts
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { outputSelector, setEmpty } from "../redux/slices/Output";
+import { outputSelector, setEmpty, setError, setModel } from "../redux/slices/Output";
 import { UIStatusSelector } from "../redux/slices/UIStatus";
 import Utils from "../lib/utils";
+import { IOutputData } from "../lib/LoideAPIInterfaces";
+import { useCallback } from "react";
 
 const useOutput = () => {
   const dispatch = useDispatch();
@@ -9,24 +11,35 @@ const useOutput = () => {
   const { model, error } = useSelector(outputSelector);
   const { newOutput } = useSelector(UIStatusSelector);
 
-  const resetNewOutput = () => {
+  const resetNewOutputBadge = useCallback(() => {
     Utils.removeNewOutputBadge();
-  };
+  }, []);
 
-  const setNewOutput = () => {
+  const setNewOutputBadge = useCallback(() => {
     Utils.addNewOutputBadge();
-  };
+  }, []);
 
-  const clearOutput = () => {
+  const clearOutput = useCallback(() => {
     dispatch(setEmpty());
-  };
+  }, [dispatch]);
+
+  const setNewOutput = useCallback(
+    (output: IOutputData) => {
+      // Dispatch action to set new model output
+      dispatch(setModel(output.model));
+      dispatch(setError(output.error));
+      setNewOutputBadge();
+    },
+    [dispatch, setNewOutputBadge],
+  );
 
   return {
     model,
     error,
     newOutput,
     clearOutput,
-    resetNewOutput,
+    resetNewOutputBadge,
+    setNewOutputBadge,
     setNewOutput,
   };
 };
